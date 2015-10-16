5c459d13c53600990af48aa644d0bde825b9d490
amykangweb <selene6023@gmail.com>
2015-10-16 13:40:35 -0700

    General code clean up and clear form on log in.

--- app/assets/javascripts/controllers/store_ctrl.js
+++ app/assets/javascripts/controllers/store_ctrl.js
@@ -17,47 +17,48 @@
 
     };
 
-  $scope.flash = function(){
-    if ($scope.username != "none") {
-      return true;
-    }else{
-      return false;
-    }
-  };
+    $scope.flash = function(){
+      if ($scope.username != "none") {
+        return true;
+      }else{
+        return false;
+      }
+    };
 
-  $scope.isAdmin = function(){
-    if($scope.admin === "true") {
-      return true;
-    }else{
-      return false;
-    }
-  };
+    $scope.isAdmin = function(){
+      if($scope.admin === "true") {
+        return true;
+      }else{
+        return false;
+      }
+    };
 
    $scope.session = function(user){
-      $http.post('api/sessions' + "?username=" + user.username + "&" + "password=" + user.password).success(function(data){
+      $http.post('api/sessions?username=' + user.username + "&password=" + user.password).success(function(data){
         $cookies.put('loggedin', 'true');
         $cookies.put('username', data.user.username);
         $cookies.put('admin', data.user.admin);
         $cookies.put('token', data.token);
-        $scope.username = $cookies.get('username');
-        $scope.token = $cookies.get('token');
+        $scope.username = data.user.username;
+        $scope.token = data.token;
         $scope.admin = $cookies.get('admin');
+        $scope.user = {};
       }).error(function(data){
         alert("Something went wrong.");
       });
     };
 
     $scope.logout = function(username){
-      $http.delete('api/sessions' + '?username=' + $scope.username).success(function(data){
+      $http.delete('api/sessions?username=' + $scope.username).success(function(data){
         $scope.username = "none";
         $cookies.remove('loggedin');
         $cookies.put('username', 'none');
         $cookies.remove('token');
-        $cookies.put('admin', 'false');
+        $cookies.remove('admin');
         $scope.loggedIn = $cookies.get('loggedin');
         $scope.username = $cookies.get('username');
         $scope.token = $cookies.get('token');
-        $scope.admin = $cookies.get('admin');
+        $scope.admin = 'false';
       }).error(function(data) {
         alert("Something went wrong.");
       });
@@ -71,7 +72,7 @@
     };
 
     $scope.create = function(product) {
-        $http.post('api/products' + "?" + "api_secret=" + $scope.token + "&" + "username=" + $scope.username, { product: product })
+        $http.post("api/products?api_secret=" + $scope.token + "&username=" + $scope.username, { product: product })
           .success(function(data) {
             $scope.products.push(data.product);
             $scope.newGem.$setPristine();
