619a7fd5791802d239f4dc23e23a1deb9d975989
amykangweb <selene6023@gmail.com>
2015-10-24 22:57:13 -0700

    Add products controller tests for non-admin users.

--- spec/controllers/products/destroy_product_spec.rb
+++ spec/controllers/products/destroy_product_spec.rb
@@ -5,13 +5,20 @@ describe Api::ProductsController do
   before do
     @gem = FactoryGirl.build(:gem)
     @gem.save
-    allow(@controller).to receive(:create_product?).and_return(true)
   end
 
   it "deleting gem is successful when user is admin" do
+    allow(@controller).to receive(:create_product?).and_return(true)
     delete :destroy, :id => @gem.id
     expect(response).to be_successful
     data = JSON.parse(response.body)
     expect(data).to have_content('"notice"=>"Gem successfully destroyed."')
+  end
+
+  it "deleting gem is unsuccessful when user is not admin" do
+    delete :destroy, :id => @gem.id
+    expect(response).to_not be_successful
+    data = JSON.parse(response.body)
+    expect(data).to have_content('"error"=>"Could not update gem, check validations"')
   end
 end
--- spec/controllers/products/update_product_spec.rb
+++ spec/controllers/products/update_product_spec.rb
@@ -5,14 +5,19 @@ describe Api::ProductsController do
   before do
     @gem = FactoryGirl.build(:gem)
     @gem.save
-    allow(@controller).to receive(:create_product?).and_return(true)
   end
 
   it "updating gem is successful when user is admin" do
+    allow(@controller).to receive(:create_product?).and_return(true)
     put :update, :id => @gem.id, :product => { :name => "Onyx", :price => 2 }
     expect(response).to be_successful
     data = JSON.parse(response.body)
     expect(data).to have_content('"name"=>"Onyx"')
     expect(data).to have_content('"price"=>2')
+  end
+
+  it "creating gem is unsuccessful when user is not admin" do
+    put :update, :id => @gem.id, :product => { :name => "Onyx", :price => 2 }
+    expect(response).to_not be_successful
   end
 end
